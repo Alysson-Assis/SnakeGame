e3dd6854e5d95152350f66af8ad7dd387632d3f8
Alysson Assis <alyssonassissantos@gmail.com>
2023-05-21 15:37:27 -0300

    O codigo esta terminado

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from turtle import Screen
 from food import Food
 from snake import Snake
-
+from scoreboard import Score
 import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.bgcolor("gray")
+screen.bgcolor("black")
 screen.title("Snake Game")
-screen.tracer(n=0)
+screen.tracer(0)
 
 snake = Snake()
 food = Food()
-
+scoreboard = Score()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -24,7 +24,24 @@ gameIsOn = True
 while gameIsOn:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    #Comida e crescimento e pontuação
+    if snake.head.distance(food) < 16:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detectando colisão
+    if snake.head.xcor() > 295 or snake.head.xcor() < -295 or snake.head.ycor() > 295 or snake.head.ycor() < -295:
+        gameIsOn = False
+        scoreboard.game_over()
+
+    #Colisão com o corpo
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            gameIsOn = False
+            scoreboard.game_over()
+
 
 screen.exitonclick()
